f6bbb628875e176d37dc6108d7286727e06bb3c6
Chris Arnault <chris.arnault.1@gmail.com>
2023-01-16 19:42:46 +0100

    colors.py: ajoute un fichier de configuration pour les pastilles Ce fichier de configuration est créé s'il n'existe pas, mais ensuite, la configuration est relue automatiquement le fichier de configuration est spécifique à la configuration du nomre de pastilles pastilles<n>.data
    La création du fichier de configuration propose par défaut la configuration de chaque pastille dans l'ordre, mais on peut à chaque étape, on petut reprendre la séquence en spécifiant un numéro
    le format pour chaque pastille est : "(\d+).(\d+).(\d+).(\d+)."

--- TCS34725/colors.py
+++ TCS34725/colors.py
@@ -1,3 +1,5 @@
+import os
+import re
 from machine import SoftI2C, Pin
 from tcs34725 import TCS34725
 
@@ -9,52 +11,125 @@ capteurRGB = TCS34725(i2c)
 capteurRGB.gain(16)  # 1 4 16 60
 capteurRGB.integration_time(2.4)  # 2.4 24 101 154 700
 
-pastilles = [(63, 61, 37),
-             (62, 68, 49),
-             (71, 78, 61),
-             (62, 77, 37),
-             (57, 78, 55),
-             (70, 84, 65),
-             (68, 90, 42),
+pastilles = dict()
 
-             (6, 68, 90),
-             (80, 107, 79),
-             (107, 64, 39),
-             (92, 60, 41),
-             (81, 75, 59),
-             (133, 112, 46),
-             (111, 101, 63),
 
-             (90, 98, 72),
-             (86, 103, 44),
-             (85, 106, 53),
-             (110, 131, 88),
-             (149, 70, 43),
-             (148, 71, 48),
-             (146, 70, 56),
+def configure_pastilles(nombre_pastilles):
+    p = 0
+    while True:
+        print("sélecte une pastille: [{}] (0..{})".format(p, nombre_pastilles - 1))
+        x = input()
+        try:
+            if len(x) == 0:
+                pastille = p
+            else:
+                pastille = int(x)
+                p = pastille
+                if pastille < 0 or pastille >= nombre_pastilles:
+                    print("{} illégal".format(pastille))
+                    continue
+                if pastille in pastilles.keys():
+                    print("{} déjà choisi".format(pastille))
+                    continue
+        except:
+            break
 
-             (170, 98, 47),
-             (162, 90, 57),
-             (159, 91, 68),
-             (200, 163, 62),
-             (201, 167, 67),
-             ]
+        print("pastille=", pastille)
 
-while True:
-    r, g, b, c = capteurRGB.read(True)
-    lux = capteurRGB.read(False)
-    print(r, g, b)
+        p += 1
 
+        r, g, b, c = capteurRGB.read(True)
+        lux = capteurRGB.read(False)
+        print(r, g, b)
+
+        pastilles[pastille] = (r, g, b)
+
+        if len(list(pastilles.keys())) >= nombre_pastilles:
+            break
+
+    return pastilles
+
+
+def check_pastille(r, g, b):
     found = False
     d = 5
-    for n, pastille in enumerate(pastilles):
-        rr, gg, bb = pastille
+    for pastille in pastilles:
+        rr, gg, bb = pastilles[pastille]
         # print(n, rr,gg,bb)
         if (rr - d) < r < (rr + d) and (gg - d) < g < (gg + d) and (bb - d) < b < (bb + d):
-            print(n, rr, gg, bb)
+            print("OK", pastille, "(", rr, gg, bb, ")")
             found = True
             break
 
     if not found:
-        print("???", r, g, b)
+        # print("???", r, g, b)
+        pastille = None
+
+    return pastille
+
+
+def test_lecture():
+    old = None
+    while True:
+        while True:
+            r, g, b, _ = capteurRGB.read(True)
+            # lux = capteurRGB.read(False)
+            if old == None:
+                old = (r, g, b)
+
+            if r == old[0] and g == old[1] and b == old[2]:
+                continue
+
+            break
+
+        pastille = check_pastille(r, g, b)
+        if pastille != None:
+            print("OK", pastille, "(", r, g, b, ")")
+
+        # print(r, g, b)
+        old = (r, g, b)
+
+
+def save_configuration(fname, pastilles):
+    with open(fname, "w") as f:
+        for p in pastilles:
+            f.write("{}[{},{},{}]\n".format(p, pastilles[p][0], pastilles[p][1], pastilles[p][2]))
+
+
+def read_configuration(fname):
+    data = dict()
+    with open(fname, "r") as f:
+        while True:
+            line = f.readline()
+            if len(line) == 0:
+                break
+
+            # print("line = ", line)
+
+            m = re.match("(\d+).(\d+).(\d+).(\d+).", line)
+            # print(m.group(1), m.group(2), m.group(3), m.group(4))
+            data[int(m.group(1))] = (int(m.group(2)), int(m.group(3)), int(m.group(4)))
+    return data
+
+
+n = 8
+fname = "pastilles{}.data".format(n)
+
+has_data = False
+try:
+    p = os.stat(fname)
+    print("Data ok")
+    has_data = True
+    pastilles = read_configuration(fname)
+except:
+    print("Data not ok !!")
+
+if not has_data:
+    pastilles = configure_pastilles(8)
+    save_configuration(fname, pastilles)
+
+print("pastilles=", pastilles)
+
+test_lecture()
+
 
