6ecb7777607437d265d810c1bca844a43fa56cc6
Chris Arnault <chris.arnault.1@gmail.com>
2023-01-16 14:16:58 +0100

    Pastilles: excursion de la couleur à partir d'un gris min et jusqu'à un gris max

--- TCS34725/pastilles.py
+++ TCS34725/pastilles.py
@@ -4,18 +4,35 @@ top = tk.Tk()
 
 c = tk.Canvas(top, bg="white", height=1000, width=1600)
 
+"""
+On va générer des pastilles de couleur
+La gamme de couleurs est parcourue entre (0, 0, 0) et (255, 255, 255)
+Avec N pas (N = 1 ou 2 ou 3 ...)
+chaque pastille est un cercle de diamètre "diam"
+il y a un espace entre chaque pastille "espace"
+si rgb = (0,0,0) alors on utilise un gris (min, min, min)
+si rgb = (255,255,255) alors on utilise un gris (max, max, max)
+"""
+
 N = 1
 diam = 220
 espace = 60
+
+min = 40
+max = 220
 
 row = 0
 column = 0
 for r in range(N + 1):
     for g in range(N + 1):
         for b in range(N + 1):
+
+            # calcul de la position de la pastille:
             x = column + espace
             y = row + espace
             coord = x, y, x+diam, y+diam
+
+            # calcul de la couleur:
             d = int(256/N)
             rr = r*d
             gg = g*d
@@ -23,10 +40,19 @@ for r in range(N + 1):
             if rr >= 256: rr = 255
             if gg >= 256: gg = 255
             if bb >= 256: bb = 255
+
+            if rr == 0 and gg == 0 and bb == 0:
+                rr, gg, bb = (min, min, min)
+            elif rr == 255 and gg == 255 and bb == 255:
+                rr, gg, bb = (max, max, max)
+
+            # dessin de la pastille (on affiche le RGB)
             c.create_oval(coord, fill="#{:02X}{:02X}{:02X}".format(rr, gg, bb))
             c.create_text(x + (diam + espace)/2, y + diam + espace, text="({},{},{})".format(rr, gg, bb), fill="black", font=('Helvetica 15 bold'))
 
             print(rr, gg, bb)
+
+            # disposition répartie sur la feuille A4
             column += diam + 2*espace
             if column > (1600 - diam - 2*espace):
                 column = 0
