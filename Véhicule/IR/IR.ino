d31d89246cea4657344da2efe1712a9dd632805c
Chris Arnault <chris.arnault.1@gmail.com>
2023-04-26 23:01:06 +0200

    Doc pour le ESP8266-Mini : le proc pour la mémorisation du véhicule

---
+++ Véhicule/IR/IR.ino
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+// An IR detector/demodulator is connected to GPIO pin 14(D5 on a NodeMCU
+// board).
+// Note: GPIO 16 won't work on the ESP8266 as it does not have interrupts.
+// Note: GPIO 14 won't work on the ESP32-C3 as it causes the board to reboot.
+#ifdef ARDUINO_ESP32C3_DEV
+const uint16_t kRecvPin = 10;  // 14 on a ESP32-C3 causes a boot loop.
+#else  // ARDUINO_ESP32C3_DEV
+const uint16_t kRecvPin = 14;
+#endif  // ARDUINO_ESP32C3_DEV
+
+// kRecvPin
+
+IRrecv irrecv(D6);
+decode_results results;
+unsigned long mmm;
+int count=0; // nb d'impulsions IR reçues
+
+void setup() {
+//  ESP.lightSleep();
+
+  pinMode(GND, OUTPUT);      // board IR
+  digitalWrite(GND, LOW);
+
+  Serial.begin(115200);
+  irrecv.enableIRIn();  // Start the receiver
+  while (!Serial)  // Wait for the serial connection to be establised.
+    delay(50);
+  Serial.println();
+  Serial.print("IRrecvDemo is now running and waiting for IR message on Pin ");
+  Serial.println(kRecvPin);
+  mmm = millis();
+}
+void loop() {
+
+  if (irrecv.decode(&results)) {
+        mmm= millis();
+    // print() & println() can't handle printing long longs. (uint64_t)
+    serialPrintUint64(results.value, HEX);
+    switch (results.value){
+    }
+
+}
